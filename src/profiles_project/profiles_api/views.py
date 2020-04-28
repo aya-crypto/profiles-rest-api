ad53cb403957b4c3d5c48387758d774d94d76f9d
unknown <amohamed@MAWARDYWSDE01.Elmawardy.local>
2020-04-28 15:52:04 +0200

    router

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
+from rest_framework import viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -50,3 +51,18 @@ class HelloApiView(APIView):
         """delete an object"""
 
         return Response({'method':'delete'})
+
+
+
+class HelloViewSet(viewsets.ViewSet):
+    """ test API ViewSet"""
+
+    def list(self, request):
+        """return hello message"""
+        a_viewset=[
+            'Uses actions (list,create,retrieve ,update ,partial update)',
+            'Automatically maps URLS using Routers',
+            "Provides more functionality with less code",
+        ]
+
+        return Response({'message':'Hello','a_viewset':a_viewset})
