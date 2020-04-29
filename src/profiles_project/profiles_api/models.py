5268beba941da757550d27a2741f546c9944c8fc
unknown <amohamed@MAWARDYWSDE01.Elmawardy.local>
2020-04-29 16:35:12 +0200

    login

--- src/profiles_project/profiles_api/models.py
+++ src/profiles_project/profiles_api/models.py
@@ -68,3 +68,16 @@ class UserProfile(AbstractBaseUser,PermissionsMixin):
     # def __init__(self, arg):
     #     super(User, self).__init__()
     #     self.arg = arg
+
+
+class ProfileFeedItem(models.Model):
+    """ profile status update"""
+
+    user_profile = models.ForeignKey('UserProfile',on_delete = models.CASCADE)
+    status_text = models.CharField(max_length=255)
+    created_on =models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Return the model as a string """
+
+        return self.status_text
